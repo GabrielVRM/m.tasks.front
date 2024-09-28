8bcf2df32e493b6faff61f9fcf08355b2f54b0be
GabrielVRM <gabriel.vieira2595@gmail.com>
2024-09-28 12:07:03 -0300

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['dayjs', 'dayjs/locale/pt-BR'], // Adicione dayjs e a localidade ao external
+    },
+  },
 })
